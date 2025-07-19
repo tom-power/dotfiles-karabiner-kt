66dcdf64805d3510d4e9f4600c830a2b398538d2
Tom Power <tp21@tuta.io>
2025-07-19 17:58:41 +0100

    add macbook

--- app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles.kt
+++ app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles.kt
@@ -1,6 +1,7 @@
 package se.tp21.dotfiles.karabiner
 
 import se.tp21.dotfiles.karabiner.profiles.k12
+import se.tp21.dotfiles.karabiner.profiles.macbook
 import se.tp21.dotfiles.karabiner.profiles.shinobi
 import se.tp21.dotfiles.karabiner.profiles.shura
 import se.tp21.dotfiles.karabiner.profiles.yoda
@@ -8,8 +9,9 @@ import sh.kau.karabiner.Profile
 
 fun profiles(): Map<String, Profile> =
     mapOf(
-        "shura" to shura(),
         "k12" to k12(),
+        "macbook" to macbook(),
         "shinobi" to shinobi(),
+        "shura" to shura(),
         "yoda" to yoda(),
     )
---
+++ app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles/macbook.kt
@@ -0,0 +1,66 @@
+package se.tp21.dotfiles.karabiner.profiles
+
+import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLike
+import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLikeExtrasNavigation
+import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLikeExtrasSymbols
+import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLikeExtrasTerminal
+import sh.kau.karabiner.*
+
+fun macbook(): Profile =
+    Profile(
+        name = "macbook",
+        complexModifications = ComplexModifications(
+            rules =
+                rightShiftViLike().rules
+                    + rightShiftViLikeExtrasNavigation().rules
+                    + rightShiftViLikeExtrasSymbols().rules
+                    + rightShiftViLikeExtrasTerminal().rules
+        ),
+        devices = listOf(
+            DeviceConfiguration(
+                identifiers = DeviceIdentifier(
+                    isKeyboard = true,
+                    productId = 835,
+                    vendorId = 1452
+                ),
+                ignore = true
+            ),
+            DeviceConfiguration(
+                identifiers = DeviceIdentifier(
+                    isKeyboard = true,
+                    productId = 24647,
+                    vendorId = 6127
+                ),
+                simpleModifications = listOf(
+                    SimpleModification(
+                        from =
+                            SimpleModificationKey(
+                                keyCode = ModifierKeyCode.LeftOption,
+                            ),
+                        to =
+                            listOf(
+                                SimpleModificationValue(
+                                    keyCode = ModifierKeyCode.LeftCommand,
+                                )
+                            )
+                    ),
+                    SimpleModification(
+                        from =
+                            SimpleModificationKey(
+                                keyCode = ModifierKeyCode.LeftCommand,
+                            ),
+                        to =
+                            listOf(
+                                SimpleModificationValue(
+                                    keyCode = ModifierKeyCode.LeftOption,
+                                )
+                            )
+                    )
+                )
+            ),
+        ),
+        virtualHidKeyboard = VirtualHidKeyboard(
+            countryCode = 2,
+            keyboardType = "iso"
+        )
+    )
