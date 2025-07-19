553c1da8f4e6e0c1fd70f76c9652d136b804b737
Tom Power <tp21@tuta.io>
2025-07-19 15:18:12 +0100

    fixes

--- app/src/test/kotlin/se/tp21/dotfiles/karabiner/DotfilesProfilesTest.kt
+++ app/src/test/kotlin/se/tp21/dotfiles/karabiner/DotfilesProfilesTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import se.tp21.dotfiles.karabiner.profiles.profiles
-import se.tp21.rightshiftvilike.karabiner.keyToFileName
 import sh.kau.karabiner.Profile
 import sh.kau.karabiner.json
 import kotlin.test.assertEquals
