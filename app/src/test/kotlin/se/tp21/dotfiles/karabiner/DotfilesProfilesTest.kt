3f65c9dcb9b277218418f41f13e2465c166c7a48
Tom Power <tpower21@gmail.com>
2025-07-20 12:52:43 +0100

    warning

--- app/src/test/kotlin/se/tp21/dotfiles/karabiner/DotfilesProfilesTest.kt
+++ app/src/test/kotlin/se/tp21/dotfiles/karabiner/DotfilesProfilesTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import sh.kau.karabiner.Profile
 import sh.kau.karabiner.json
-import java.io.File
 import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
