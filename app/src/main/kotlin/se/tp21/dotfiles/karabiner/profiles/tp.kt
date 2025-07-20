86bb3229ef1359fe8bd21762a0a3b69ef06347d3
Tom Power <tpower21@gmail.com>
2025-07-20 09:08:00 +0100

    add tp

---
+++ app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles/tp.kt
@@ -0,0 +1,19 @@
+package se.tp21.dotfiles.karabiner.profiles
+
+import se.tp21.dotfiles.karabiner.profiles.utils.base
+import se.tp21.dotfiles.karabiner.profiles.utils.keyboardIso
+import se.tp21.dotfiles.karabiner.profiles.utils.deviceConfigurationMacbook
+import se.tp21.sixtypercent.karabiner.modifications.sixtyPercent
+import sh.kau.karabiner.*
+
+fun tp(): Profile =
+    Profile(
+        name = "tp",
+        complexModifications = ComplexModifications(
+            rules = base() + sixtyPercent().rules
+        ),
+        devices = listOf(
+            deviceConfigurationMacbook()
+        ),
+        virtualHidKeyboard = keyboardIso()
+    )
