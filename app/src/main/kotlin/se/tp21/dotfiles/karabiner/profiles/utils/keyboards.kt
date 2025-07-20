9d666a67bf802275c3a61bf30cb8bc6a3c17c4b1
Tom Power <tp21@tuta.io>
2025-07-20 11:20:08 +0100

    drop the other profiles

--- app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles/utils/keyboards.kt
+++ app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles/utils/keyboards.kt
@@ -2,12 +2,6 @@ package se.tp21.dotfiles.karabiner.profiles.utils
 
 import sh.kau.karabiner.VirtualHidKeyboard
 
-fun keyboardIso(): VirtualHidKeyboard =
-    VirtualHidKeyboard(
-        countryCode = 2,
-        keyboardType = "iso"
-    )
-
 fun keyboardAnsi(): VirtualHidKeyboard =
     VirtualHidKeyboard(
         countryCode = 2,
