1c1737ae445b4798ae8b6e67114bde45a482de18
Tom Power <tpower21@gmail.com>
2025-07-20 10:16:36 +0100

    tidy

---
+++ app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles/devices/utils.kt
@@ -0,0 +1,22 @@
+package se.tp21.dotfiles.karabiner.profiles.devices
+
+import sh.kau.karabiner.Condition
+import sh.kau.karabiner.DeviceIdentifier
+import sh.kau.karabiner.KarabinerRule
+import sh.kau.karabiner.Manipulator
+
+fun List<KarabinerRule>.withDeviceIfConditionFor(identifier: DeviceIdentifier): List<KarabinerRule> {
+    val deviceIfCondition = Condition.DeviceIfCondition(listOf(identifier))
+    return this.map { rule ->
+        rule.copy(
+            manipulators =
+                rule.manipulators?.map { manipulator ->
+                    manipulator.withDeviceIfCondition(deviceIfCondition)
+                }
+        )
+    }
+}
+
+private fun Manipulator.withDeviceIfCondition(deviceIfCondition: Condition.DeviceIfCondition): Manipulator {
+    return this.copy(conditions = this.conditions?.plus(listOf<Condition>(deviceIfCondition)))
+}
