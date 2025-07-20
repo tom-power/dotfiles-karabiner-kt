7a37cf203137f81e89bb9ddf30d531de876b32ac
Tom Power <tp21@tuta.io>
2025-07-20 22:57:12 +0100

    tidy

--- app/src/main/kotlin/se/tp21/dotfiles/karabiner/Main.kt
+++ app/src/main/kotlin/se/tp21/dotfiles/karabiner/Main.kt
@@ -2,18 +2,13 @@ package se.tp21.dotfiles.karabiner
 
 import se.tp21.karabiner.snippets.SnippetRules
 import se.tp21.karabiner.snippets.complexModificationsFrom
+import sh.kau.karabiner.ComplexModifications
 import sh.kau.karabiner.json
 import java.io.File
 
 fun main() {
     try {
-        val snippetRulesJson = File("src/main/resources/snippetRules.json").inputStream().bufferedReader().use { it.readText() }
-
-        val snippetRules = json().decodeFromString<SnippetRules>(snippetRulesJson)
-        val snippetModifications = complexModificationsFrom(snippetRules)
-
-        val karabinerJson = json().encodeToString(karabinerConfig(snippetModifications.rules))
-
+        val karabinerJson = json().encodeToString(karabinerConfig(snippetModifications().rules))
         File("build/karabiner.json").let {
             it.writeText(karabinerJson)
             println("Successfully wrote json to ${it.absolutePath}")
@@ -22,4 +17,11 @@ fun main() {
         System.err.println("Error writing json: ${e.message}")
         e.printStackTrace()
     }
+}
+
+private fun snippetModifications(): ComplexModifications {
+    val snippetRulesJson =
+        File("src/main/resources/snippetRules.json").inputStream().bufferedReader().use { it.readText() }
+    val snippetRules = json().decodeFromString<SnippetRules>(snippetRulesJson)
+    return complexModificationsFrom(snippetRules)
 }
