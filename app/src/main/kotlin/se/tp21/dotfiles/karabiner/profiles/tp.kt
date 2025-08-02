f92bbc68b205ed74f680d9852798fb745d05c641
Tom Power <tp21@tuta.io>
2025-08-02 06:50:49 +0100

    drop sixtyPercentShura

--- app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles/tp.kt
+++ app/src/main/kotlin/se/tp21/dotfiles/karabiner/profiles/tp.kt
@@ -1,15 +1,6 @@
 package se.tp21.dotfiles.karabiner.profiles
 
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceConfigurationK12
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceConfigurationMacbookAir
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceConfigurationMacbookPro
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceConfigurationShinobi
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceConfigurationShura
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceConfigurationYoda
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceIfRulesK12
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceIfRulesShura
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceIfRulesSixtyPercent
-import se.tp21.dotfiles.karabiner.profiles.devices.deviceIfRulesYoda
+import se.tp21.dotfiles.karabiner.profiles.devices.*
 import se.tp21.dotfiles.karabiner.profiles.utils.baseRules
 import se.tp21.dotfiles.karabiner.profiles.utils.keyboardAnsi
 import se.tp21.dotfiles.karabiner.profiles.utils.simpleModificationsGlobal
@@ -26,7 +17,6 @@ fun tp(snippetRules: List<KarabinerRule>): Profile =
                 rules =
                     baseRules()
                         + deviceIfRulesSixtyPercent()
-                        + deviceIfRulesShura()
                         + deviceIfRulesK12()
                         + deviceIfRulesYoda()
                         + snippetRules
