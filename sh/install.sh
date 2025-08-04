18769be1c5ea040da039ed892b48b08e724a7594
Tom Power <tpower21@gmail.com>
2025-08-04 12:56:59 +0100

    cp -v will echo

--- sh/install.sh
+++ sh/install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 ./gradlew build &&
-cp ./app/build/karabiner.json ~/.config/karabiner/karabiner.json &&
-echo "Successfully copied json to $HOME/.config/karabiner/karabiner.json"
+cp -v ./app/build/karabiner.json ~/.config/karabiner/karabiner.json
